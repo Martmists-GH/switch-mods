8ca2e75e5cdf5d6e875763dde7873d3a6cf4e5de
Martmists <martmists@gmail.com>
2026-03-18 23:49:04 +0100

    remove file from comm

--- src/pokemon_sv/commission_vaulttecalex101/romfs/script/lua/boot/50_spawn_patch.lua
+++
@@ -1,8 +0,0 @@
----main.encount.EncountCreatePokepara:Create
-_trampoline(C2B428A63695FD2C7, "S988D7D0A4EEF693D", function(old, rand, dev, form, sexType, level, pokeData, rareTryUp, option)
-    if dev == 641 or dev == 642 or dev == 645 then
-        return old(rand, dev, form, 1, level, pokeData, rareTryUp, option)
-    else
-        return old(rand, dev, form, 2, level, pokeData, rareTryUp, option)
-    end
-end)
